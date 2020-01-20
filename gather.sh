e618c3e2b28a39953f4dc181439bd4d00cb25605
Michael Hyland <michael.hyland@kurtosys.com>
2020-01-20 19:34:39 +0200

    auto-commit from michael.hyland@SAMAC0120 on Mon Jan 20 19:34:39 SAST 2020

--- gather.sh
+++ gather.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-gather/au_news.py
-gather/ca_news.py
-gather/nz_news.py
-gather/gb_news.py
-gather/us_news.py
-gather/za_news.py
+#gather/au_news.py
+#gather/ca_news.py
+#gather/nz_news.py
+#gather/gb_news.py
+#gather/us_news.py
+#gather/za_news.py
+
+message="auto-commit from $USER@$(hostname -s) on $(date)"
+GIT=`which git`
+#REPO_DIR=~/org
+#cd ${REPO_DIR}
+${GIT} add --all .
+${GIT} commit -m "$message"
+
+gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
+echo "$gitPush"
--- news/__init__.py
+++ news/__init__.py
@@ -10,8 +10,19 @@ app.config['SECRET_KEY'] = 'CGVC39xyCk8lUmR6DzT_LA'
 
 db = SQLAlchemy(app)
 
-from news.za.routes import za
 from news.main.routes import main
+from news.za.routes import za
+from news.us.routes import us
+from news.au.routes import au
+from news.ca.routes import ca
+from news.nz.routes import nz
+from news.gb.routes import gb
+
 
 app.register_blueprint(za)
 app.register_blueprint(main)
+app.register_blueprint(us)
+app.register_blueprint(au)
+app.register_blueprint(ca)
+app.register_blueprint(nz)
+app.register_blueprint(gb)
