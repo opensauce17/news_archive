864bc2135363665e4731d1a223d19e770706d7e3
Michael Hyland <michael.hyland@kurtosys.com>
2020-01-20 08:44:43 +0200

    updates

--- news/main/routes.py
+++ news/main/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, url_for, redirect, message_flashed, flash, jsonify, session, Blueprint
-
+from datetime import datetime
+from sqlalchemy import and_
+from news.models import za_news
 
 main = Blueprint('main', __name__)
 
@@ -16,7 +18,16 @@ def country():
     country = request.args.get('country')
 
     if country == "za":
-        return render_template('za/index.html')
+
+        today = datetime.now().strftime("%d %B, %Y")
+        today_date = datetime.now()
+        today_date = str(today_date).split(" ", 1)[0]
+        headlines = za_news.query.filter(
+            and_(za_news.news_type == 'headlines', za_news.publishedAt.like(f'%{today_date}%'))) \
+            .order_by(za_news.publishedAt.desc())
+        news_type = 'headlines'
+
+        return render_template('za/index.html', headlines=headlines, today=today, news_type=news_type)
 
     if country == "us":
         return render_template('za/index.html')
