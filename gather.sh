dfff9377d3d273508a744fb8b34548982d44d8cc
Michael Hyland <michael.hyland@kurtosys.com>
2020-01-21 09:39:42 +0200

    updated gather/git script

--- gather.sh
+++ gather.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-/opt/news_archive/gather/au_news.py
+#/opt/news_archive/gather/au_news.py
 #/opt/news_archive/gather/ca_news.py
 #/opt/news_archive/gather/nz_news.py
 #/opt/news_archive/gather/gb_news.py
 #/opt/news_archive/gather/us_news.py
 #/opt/news_archive/gather/za_news.py
 
-#message="auto-commit from opensauce17  on $(date)"
-#GIT=`which git`
-#REPO_DIR=/opt/news_archive
-#cd ${REPO_DIR}
-#${GIT} add --all .
-#${GIT} commit -m "$message"
+message="auto-commit from Mike Hyland  on $(date)"
+GIT=`which git`
+REPO_DIR=/opt/news_archive
+cd ${REPO_DIR}
+${GIT} add --all .
+${GIT} commit -m "$message"
+${GIT} checkout master
+${GIT} merge deploy
+${GIT} merge push
+${GIT} checkout deploy
 
-#gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
-#echo "$gitPush"
+gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
+echo "$gitPush"
