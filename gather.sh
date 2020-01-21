ba422ca83ac99b2ce254d208610890f16cb0ed06
Mike Hyland <opensauce17@gmail.com>
2020-01-21 07:27:29 +0000

    updates from dev machine

--- gather.sh
+++ gather.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 /opt/news_archive/gather/au_news.py
-/opt/news_archive/gather/ca_news.py
-/opt/news_archive/gather/nz_news.py
-/opt/news_archive/gather/gb_news.py
-/opt/news_archive/gather/us_news.py
-/opt/news_archive/gather/za_news.py
+#/opt/news_archive/gather/ca_news.py
+#/opt/news_archive/gather/nz_news.py
+#/opt/news_archive/gather/gb_news.py
+#/opt/news_archive/gather/us_news.py
+#/opt/news_archive/gather/za_news.py
 
-message="auto-commit from $USER@$(hostname -s) on $(date)"
-GIT=`which git`
-REPO_DIR=/opt/news_archive
+#message="auto-commit from opensauce17  on $(date)"
+#GIT=`which git`
+#REPO_DIR=/opt/news_archive
 #cd ${REPO_DIR}
-${GIT} add --all .
-${GIT} commit -m "$message"
+#${GIT} add --all .
+#${GIT} commit -m "$message"
 
-gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
-echo "$gitPush"
+#gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
+#echo "$gitPush"
