941d0958119e48a04a5883c55727d5945c941613
Michael Hyland <michael.hyland@kurtosys.com>
2020-02-18 10:34:53 +0200

    updates

--- gather.sh
+++ gather.sh
@@ -7,18 +7,6 @@
 /opt/news_archive/gather/us_news.py
 /opt/news_archive/gather/za_news.py
 
-#message="auto-commit from Mike Hyland  on $(date)"
-#GIT=`which git`
-#REPO_DIR=/opt/news_archive
-#cd ${REPO_DIR}
-#${GIT} config user.email "opensauce17@gmail.com"
-#${GIT} config user.name "Mike Hyland"
-#${GIT} add --all .
-#${GIT} commit -m "$message"
-#${GIT} checkout master
-#${GIT} merge deploy
-#${GIT} merge push
-#${GIT} checkout deploy
+/usr/local/bin/aws s3 rm s3://newsarchive-db-backup/db.db
 
-#gitPush=$(${GIT} push -vvv git@github.com:opensauce17/news_archive.git master 2>&1)
-#echo "$gitPush"
+/usr/local/bin/aws  s3 cp /opt/news_archive/news/db/db.db s3://newsarchive-db-backup
