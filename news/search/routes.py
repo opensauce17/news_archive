d58c473463c9a856a84f3b7a41d569efb8a98caa
Michael Hyland <michael.hyland@kurtosys.com>
2020-08-18 18:37:46 +0200

    updates

--- news/search/routes.py
+++ news/search/routes.py
@@ -1435,45 +1435,18 @@ def all_search():
 
     search = request.args.get('search_item')
 
-    everything_search = []
+    au_everything_search = au_news.query.filter(and_(au_news.description.contains(search) | au_news.title.contains(search))).order_by(au_news.publishedAt.desc())
 
-    au_search_everything = au_news.query.filter(and_(au_news.description.contains(search) | au_news.title.contains(search))).order_by(au_news.publishedAt.desc())
+    ca_everything_search = ca_news.query.filter(and_(ca_news.description.contains(search) | ca_news.title.contains(search))).order_by(ca_news.publishedAt.desc())
 
-    everything_search.append(au_search_everything)
+    gb_everything_search = gb_news.query.filter(and_(gb_news.description.contains(search) | gb_news.title.contains(search))).order_by(gb_news.publishedAt.desc())
 
-    ca_search_everything = ca_news.query.filter(and_(ca_news.description.contains(search) | ca_news.title.contains(search))).order_by(ca_news.publishedAt.desc())
+    nz_everything_search = nz_news.query.filter(and_(nz_news.description.contains(search) | nz_news.title.contains(search))).order_by(nz_news.publishedAt.desc())
 
-    everything_search.append(ca_search_everything)
+    za_everything_search = za_news.query.filter(and_(za_news.description.contains(search) | za_news.title.contains(search))).order_by(za_news.publishedAt.desc())
 
-    gb_search_everything = gb_news.query.filter(and_(gb_news.description.contains(search) | gb_news.title.contains(search))).order_by(gb_news.publishedAt.desc())
+    us_everything_search = us_news.query.filter(and_(us_news.description.contains(search) | us_news.title.contains(search))).order_by(us_news.publishedAt.desc())
 
-    everything_search.append(gb_search_everything)
-
-    nz_search_everything = nz_news.query.filter(and_(nz_news.description.contains(search) | nz_news.title.contains(search))).order_by(nz_news.publishedAt.desc())
-
-    everything_search.append(nz_search_everything)
-
-    za_search_everything = za_news.query.filter(and_(za_news.description.contains(search) | za_news.title.contains(search))).order_by(za_news.publishedAt.desc())
-
-    everything_search.append(za_search_everything)
-
-    us_search_everything = us_news.query.filter(and_(us_news.description.contains(search) | us_news.title.contains(search))).order_by(us_news.publishedAt.desc())
-
-    everything_search.append(us_search_everything)
-
-    # for i in everything_search:
-    #     for x in i:
-    #         date = x.publishedAt.replace('T', " ").replace('Z', "")
-    #         date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    #         date = date.strftime("%m/%d/%Y")
-
-    # for i in everything_search:
-    #     for x in i:
-    #         c = x.location
-    #         print(c)
-
-    return render_template('search/everything_search.html', everything_search=everything_search, search=search)
-
-    #news_type=news_type, everything_search=everything_search, country=country,
-    #                           search_everything=search_everything, date_category_country=date_category_country, search=search
+    return render_template('search/everything_search.html', au_everything_search=au_everything_search, ca_everything_search=ca_everything_search, gb_everything_search=gb_everything_search, 
+    nz_everything_search=nz_everything_search, za_everything_search=za_everything_search, us_everything_search=us_everything_search, search=search)
 
